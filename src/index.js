ce103c4eb67272089784ff3bd2afbe6d46dd76f4
Anthony Dickerson <deoism@gmail.com>
2017-11-16 21:23:29 -0500

    catching up to 10

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import ReactDOM from 'react-dom';
-import React from 'react';
+import React from "react";
+import { Provider } from "react-redux";
+import {Router,Route,IndexRoute,hashHistory} from "react-router";
 
-class App extends React.Component {
-  render() {
-    return (
-      <h1>Hello, World!</h1>
-    );
-  }
-}
+import store from 	"./store";
+import App from 	"./components/Apps";
+import Home from 	"./components/Home";
+import Login from 	"./components/Login";
+import Register from "./components/Register";
+import Settings from "./components/Settings";
 
-ReactDOM.render((
-  <App />
-), document.getElementById('root'));
+
+
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={hashHistory}>
+			<Route path="/" component={App}>
+				<IndexRoute component={Home} />
+				<Route path="/login" component={Login} />
+				<Route path="/register" component={Register} />
+				<Route path="/settings" component={Settings} />
+			</Route>
+		</Router>
+	</Provider>,
+		document.getElementById("root")
+);
