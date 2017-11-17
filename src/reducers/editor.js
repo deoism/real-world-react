fc5d40accf20de5ac1ed844c51332167ace439b0
Anthony Dickerson <deoism@gmail.com>
2017-11-17 15:36:13 -0500

    repaing botched copy

--- src/reducers/editor.js
+++ src/reducers/editor.js
@@ -1,19 +1,17 @@
-
-
 export default (state = {}, action) => {
-    swtich (action.type) {
-        case "ARTICLE_SUBMITTED":
-            return{
-                ...state,
-                inProgresss: null,
-                errors: actiono.error ? action.payload.errors : null
-            };
-        case "ASYNC_START":
-            if (action.subtype === "ARTICLE_SUBMITTED"){
-                return { ...state, inProgress: true};
-            }
-            return state;
-            default:
-                return state;
+    switch (action.type) {
+      case "ARTICLE_SUBMITTED":
+        return {
+          ...state,
+          inProgress: null,
+          errors: action.error ? action.payload.errors : null
+        };
+      case "ASYNC_START":
+        if (action.subtype === "ARTICLE_SUBMITTED") {
+          return { ...state, inProgress: true };
+        }
+        return state;
+      default:
+        return state;
     }
-}; 
+  };
